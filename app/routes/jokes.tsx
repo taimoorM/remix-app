569c82d3a95c7883152e941a60ba1ff977aa41b6
Taimoor <taimoor.m89@gmail.com>
2023-02-28 00:34:00 -0500

    configured db for dev and updated jokes route

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,20 +1,56 @@
 import type { LinksFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/jokes.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
-function jokes() {
+export const loader = async () => {
+  return json({
+    jokes: await db.joke.findMany(),
+  });
+};
+
+export default function JokesRoute() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div className="container">
-      <div className="content">
-        <h1>JOKES 🤣</h1>
-        <Outlet />
-      </div>
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">
+            <Link to=".">Get a random joke</Link>
+            <p>Here are a few more jokes to check out:</p>
+            <ul>
+              {data.jokes.map((joke) => (
+                <li key={joke.id}>
+                  <Link to={joke.id}>{joke.name}</Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="new" className="button">
+              Add your own
+            </Link>
+          </div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
-export default jokes;
